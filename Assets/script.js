ac21d9f9aade4e203a32852027e4db96daea1627
Rockoban420 <lautaro.avellaneda@gmail.com>
2023-03-18 23:52:44 -0700

    almost finished/ no input check

--- Assets/script.js
+++ Assets/script.js
@@ -4,13 +4,22 @@ var buttonEl = $('#searchButton');
 var inputEl = $('#cityName');
 var citiesListEl = $('#citiesList');
 var dayWeather = $('#currentForecast');
+var placeholder = $('#placeholder');
 var citiesList = JSON.parse(localStorage.getItem("cities")) ?? {};
+var cityNameEl = document.createElement ('h1');
+var iconEl = document.createElement ('img');
+var tempEl = document.createElement ('p');
+var windEl = document.createElement ('p');
+var humidityEl = document.createElement ('p');
+var cardEl = $('.card');
+var list = 3;
 
 var appendCities = function () {
   for (const key in citiesList) {
     var cityButton = document.createElement('button');
     cityButton.innerHTML = key;
     cityButton.setAttribute('class', 'd-flex');
+    cityButton.setAttribute('id', 'cityID');
     cityButton.setAttribute('style', 'margin-left: 2%; justify-content: center; margin-top: 2%; width: 65%;')
     citiesListEl.append(cityButton);
   }
@@ -19,44 +28,97 @@ var appendCities = function () {
 function getCurrentW(requestUrl) {
   fetch(requestUrl)
     .then(function (response) {
-      console.log (response);
+      if (response.status === 404) {
+        return false;
+      }
+      else {
       return response.json();
+      }
     })
     .then(function (data) {
-      console.log(data);
+      placeholder.text  ('');
       var cityName = data.name;
-      var currentTempK = data.main.temp;
-      var currentTempN = (currentTempK-273.15) * 1.8 + 32;
-      var currentTempR = Math.floor(currentTempN);
-      let cityNameEl = document.createElement ('h1');
-      var iconEl = document.createElement ('img');
-      iconEl.src = 'https://openweathermap.org/img/w/10d.png'
+      let currentTempK = data.main.temp;
+      let currentTempN = (currentTempK-273.15) * 1.8 + 32;
+      let currentTempR = Math.floor(currentTempN);
+      let currentWind = data.wind.speed;
+      let humidityV = data.main.humidity;
+      cityNameEl.innerHTML = '';
+      iconEl.src = '';
+      tempEl.innerHTML = '';
+      iconEl.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      iconEl.setAttribute ('class' , 'd-inline');
       cityNameEl.textContent = 'Todays forecast for the city of ' + cityName;
+      cityNameEl.setAttribute ('class', 'd-inline');
       dayWeather.append (cityNameEl);
-      let tempEl = document.createElement ('p');
+      dayWeather.append (iconEl);
       tempEl.setAttribute ('style', 'margin-left: 2%;');
       tempEl.textContent = 'Current Temperature: ' + currentTempR + 'F';
+      windEl.setAttribute ('style', 'margin-left: 2%;');
+      windEl.textContent = 'Current Wind Speed: ' + currentWind + 'MPH';
+      humidityEl.setAttribute ('style', 'margin-left: 2%;');
+      humidityEl.textContent = 'Current Humidity: ' + humidityV + '%';
       dayWeather.append (tempEl);
-    })
+      dayWeather.append (windEl);
+      dayWeather.append (humidityEl);
+      var requestUrl3 = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&id=524901&appid=d6c6f2f334d8420a6173b353ed5724d3`;
+      fetch(requestUrl3)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let currentTempK;
+        let currentWind;
+        let humidityV;
+        list = 3;
+        for (let i = 1; i <= cardEl.length; i++){
+          let currentCardEl = $(`#${i}`);
+          let iconEl2 = document.createElement ('img');
+          currentWind = data.list[list].wind.speed;
+          currentTempK = data.list[list].main.temp;
+          humidityV = data.list[list].main.humidity;
+          let currentTempN = (currentTempK-273.15) * 1.8 + 32;
+          let currentTempR = Math.floor(currentTempN);
+          iconEl2.src = `https://openweathermap.org/img/w/${data.list[list].weather[0].icon}.png`;
+          let currentDate = data.list[list].dt_txt;
+          let myDateArr = currentDate.split(' ');
+          currentCardEl[0].children[0].children[0].innerText = myDateArr[0];
+          currentCardEl[0].children[0].children[0].append (iconEl2);
+          currentCardEl[0].children[0].children[1].innerText = `Temp: ${currentTempR}°F\n`;
+          currentCardEl[0].children[0].children[1].innerText += `Wind: ${currentWind}mph\n`;
+          currentCardEl[0].children[0].children[1].innerText += `Humidity: ${humidityV}%`;
+          list = list + 8;
+        }
+        });
+  });
 };
 
-function displayWeather () {
-  let cityNameEl = document.createElement ('h1');
-  cityNameEl.textContent = 'Todays forecast for the city of ' + cityName;
-  dayWeather.append (cityNameEl);
-  let tempEl = document.createElement ('p');
-  tempEl.textContent = currentTempR;
-  dayWeather.append (tempEl);
-}
-
 appendCities();
+if (Object.keys(citiesList)[0]){
+let requestUrl1 = `http://api.openweathermap.org/data/2.5/weather?q=${Object.keys(citiesList)[0]}&id=524901&appid=d6c6f2f334d8420a6173b353ed5724d3`;
+getCurrentW (requestUrl1);
+} else {
+  placeholder.text ('Current Day Forecast');
+};
 
 buttonEl.on('click', function () {
-  var currentCityName = inputEl.val();
+  let currentCityName = inputEl.val();
   citiesList[currentCityName] = 1;
   localStorage.setItem("cities", JSON.stringify(citiesList));
-  // var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${currentCityName}&id=524901&appid=d6c6f2f334d8420a6173b353ed5724d3`;
-  // getApi(requestUrl);
-  var requestUrl2 = `http://api.openweathermap.org/data/2.5/weather?q=${currentCityName}&id=524901&appid=d6c6f2f334d8420a6173b353ed5724d3`;
+  let requestUrl2 = `http://api.openweathermap.org/data/2.5/weather?q=${currentCityName}&id=524901&appid=d6c6f2f334d8420a6173b353ed5724d3`;
+  getCurrentW (requestUrl2);
+  var cityButton = document.createElement('button');
+  cityButton.innerHTML = currentCityName;
+  cityButton.setAttribute('class', 'd-flex');
+  cityButton.setAttribute('id', 'cityID');
+  cityButton.setAttribute('style', 'margin-left: 2%; justify-content: center; margin-top: 2%; width: 65%;')
+  citiesListEl.append(cityButton);
+});
+
+
+
+citiesListEl.on ('click' , function(event){
+  let currentCityName = event.target.innerHTML;
+  let requestUrl2 = `http://api.openweathermap.org/data/2.5/weather?q=${currentCityName}&id=524901&appid=d6c6f2f334d8420a6173b353ed5724d3`;
   getCurrentW (requestUrl2);
 });
